9a9bded0fdb2981159db43ad5e0d931176758a18
Kenneth Keiter <ken@kenkeiter.com>
2013-07-31 15:00:26 -0700

    Moved all card definitions into the main skeuocard.coffee file; closes #16.

--- javascripts/skeuocard.js
+++ javascripts/skeuocard.js
@@ -13,7 +13,7 @@
 
 
 (function() {
-  var Skeuocard,
+  var CCIssuers, CCProducts, Skeuocard,
     __indexOf = [].indexOf || function(item) { for (var i = 0, l = this.length; i < l; i++) { if (i in this && this[i] === item) return i; } return -1; },
     __slice = [].slice,
     __hasProp = {}.hasOwnProperty,
@@ -1260,5 +1260,157 @@
   })(Skeuocard.prototype.TextInputView);
 
   window.Skeuocard = Skeuocard;
+
+  /*
+  # Card Definitions
+  */
+
+
+  CCProducts = {};
+
+  CCProducts[/^30[0-5][0-9]/] = {
+    companyName: "Diners Club",
+    companyShortname: "dinersclub",
+    cardNumberGrouping: [4, 6, 4],
+    expirationFormat: "MM/YY",
+    cvcLength: 3,
+    layout: {
+      number: 'front',
+      exp: 'front',
+      name: 'front',
+      cvc: 'back'
+    }
+  };
+
+  CCProducts[/^3095/] = {
+    companyName: "Diners Club International",
+    companyShortname: "dinersclubintl",
+    cardNumberGrouping: [4, 6, 4],
+    expirationFormat: "MM/YY",
+    cvcLength: 3,
+    layout: {
+      number: 'front',
+      exp: 'front',
+      name: 'front',
+      cvc: 'back'
+    }
+  };
+
+  CCProducts[/^35\d{2}/] = {
+    companyName: "JCB",
+    companyShortname: "jcb",
+    cardNumberGrouping: [4, 4, 4, 4],
+    expirationFormat: "MM/YY",
+    cvcLength: 3,
+    layout: {
+      number: 'front',
+      exp: 'front',
+      name: 'front',
+      cvc: 'back'
+    }
+  };
+
+  CCProducts[/^36\d{2}/] = {
+    companyName: "Diners Club International",
+    companyShortname: "dinersclubintl",
+    cardNumberGrouping: [4, 6, 4],
+    expirationFormat: "MM/YY",
+    cvcLength: 3,
+    layout: {
+      number: 'front',
+      exp: 'front',
+      name: 'front',
+      cvc: 'back'
+    }
+  };
+
+  CCProducts[/^37/] = {
+    companyName: "American Express",
+    companyShortname: "amex",
+    cardNumberGrouping: [4, 6, 5],
+    expirationFormat: "MM/YY",
+    cvcLength: 4,
+    layout: {
+      number: 'front',
+      exp: 'front',
+      name: 'front',
+      cvc: 'back'
+    }
+  };
+
+  CCProducts[/^38/] = {
+    companyName: "Hipercard",
+    companyShortname: "hipercard",
+    cardNumberGrouping: [4, 4, 4, 4],
+    expirationFormat: "MM/YY",
+    cvcLength: 3,
+    layout: {
+      number: 'front',
+      exp: 'front',
+      name: 'front',
+      cvc: 'back'
+    }
+  };
+
+  CCProducts[/^4[0-9]\d{2}/] = {
+    companyName: "Visa",
+    companyShortname: "visa",
+    cardNumberGrouping: [4, 4, 4, 4],
+    expirationFormat: "MM/YY",
+    cvcLength: 3,
+    layout: {
+      number: 'front',
+      exp: 'front',
+      name: 'front',
+      cvc: 'back'
+    }
+  };
+
+  CCProducts[/^5[0-8]\d{2}/] = {
+    companyName: "Mastercard",
+    companyShortname: "mastercard",
+    cardNumberGrouping: [4, 4, 4, 4],
+    expirationFormat: "MM/YY",
+    cvcLength: 3,
+    layout: {
+      number: 'front',
+      exp: 'front',
+      name: 'front',
+      cvc: 'back'
+    }
+  };
+
+  CCProducts[/^6011/] = {
+    companyName: "Discover",
+    companyShortname: "discover",
+    cardNumberGrouping: [4, 4, 4, 4],
+    expirationFormat: "MM/YY",
+    cvcLength: 3,
+    layout: {
+      number: 'front',
+      exp: 'front',
+      name: 'front',
+      cvc: 'back'
+    }
+  };
+
+  CCIssuers = {};
+
+  /*
+  Hack fixes the Chase Sapphire card's stupid (nice?) layout non-conformity.
+  */
+
+
+  CCIssuers[/^414720/] = {
+    issuingAuthority: "Chase",
+    issuerName: "Chase Sapphire Card",
+    issuerShortname: "chase-sapphire",
+    layout: {
+      number: 'front',
+      exp: 'front',
+      name: 'front',
+      cvc: 'front'
+    }
+  };
 
 }).call(this);
